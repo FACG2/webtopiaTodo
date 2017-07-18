9680455e8cf019c609c772ec6306c5edd50055b6
kefah <kelhelouwork@gmail.com>
2017-07-18 13:28:35 +0300

    add mark and sort functions

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,7 +1,79 @@
 var test = require('tape');
-var logic = require('./logic');
+var todoFunctions = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+
+var todoList = [
+  {id: 0, description: 'make tea', done: false},
+  {id: 1, description: 'make eggs', done: true},
+  {id: 2, description: 'make coffee', done: false},
+  {id: 3, description: 'make launch', done: true},
+  {id: 4, description: 'make dinner', done: false},
+  {id: 5, description: 'make food', done: true},
+]
+
+test('should return the same array', function (t) {
+  t.deepEqual(todoFunctions.markTodo(todoList,0),
+  [{id: 0, description: 'make tea', done: true},
+  {id: 1, description: 'make eggs', done: true},
+  {id: 2, description: 'make coffee', done: false},
+  {id: 3, description: 'make launch', done: true},
+  {id: 4, description: 'make dinner', done: false},
+  {id: 5, description: 'make food', done: true},]);
   t.end();
 });
+
+test('should return an array after marking the task', function (t) {
+  t.deepEqual(todoList,[
+    {id: 0, description: 'make tea', done: false},
+    {id: 1, description: 'make eggs', done: true},
+    {id: 2, description: 'make coffee', done: false},
+    {id: 3, description: 'make launch', done: true},
+    {id: 4, description: 'make dinner', done: false},
+    {id: 5, description: 'make food', done: true},
+  ]);
+  t.end();
+});
+
+test('should return an array after marking the task', function (t) {
+  t.deepEqual(todoFunctions.markTodo(todoList,1),
+  [{id: 0, description: 'make tea', done: false},
+  {id: 1, description: 'make eggs', done: false},
+  {id: 2, description: 'make coffee', done: false},
+  {id: 3, description: 'make launch', done: true},
+  {id: 4, description: 'make dinner', done: false},
+  {id: 5, description: 'make food', done: true},]);
+  t.end();
+});
+
+// Sort test
+
+test('should return an array after marking the task', function (t) {
+  t.deepEqual(todoFunctions.sortTodos(todoList),
+    [{id: 1, description: 'make eggs', done: true},
+    {id: 3, description: 'make launch', done: true},
+    {id: 5, description: 'make food', done: true},
+    {id: 0, description: 'make tea', done: false},
+    {id: 2, description: 'make coffee', done: false},
+    {id: 4, description: 'make dinner', done: false}
+  ]);
+  t.end();
+});
+
+
+test('should return an array after marking the task', function (t) {
+  t.deepEqual(todoFunctions.markTodo(todoList,0),
+  [{id: 0, description: 'make tea', done: true},
+  {id: 1, description: 'make eggs', done: true},
+  {id: 2, description: 'make coffee', done: false},
+  {id: 3, description: 'make launch', done: true},
+  {id: 4, description: 'make dinner', done: false},
+  {id: 5, description: 'make food', done: true},]);
+  t.end();
+});
+
+
+
+
+//   {id: 0, description: 'make tea', done: false},
+//   {id: 1, description: 'make eggs', done: false},
+// ]
